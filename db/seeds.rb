811da0aa73577f48021596356f135ba1b27a5651
Alif Momin <alifmomin1234567890@gmail.com>
2019-05-28 22:36:45 +0000

    Seed Change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(name: 'Pizza Nomster', address: '2 Main street, Cambridge, MA 02139', description: 'Best Pizza in Cambridge. Their Crust is to die for!')
+Place.create(name: "Chick Fil A", address: "321 Yummy Street, Houston, TX 77054", description: "Most delicious chicken burgers in the world!")
+Place.create(name: "Zinger", address: "213 Flaming Hot Street, Houston, TX 77054", description: "The most spicest chicken burgers in the galaxy!")
+Place.create(name: "Area 1", address: "123 Cool Street, Houston,TX 77054", description: "UFO style food available!")
+Place.create(name: "Pizza 101", address: "111 Yummy Street, Houston, TX 77054", description: "Well rounded pizza for a low price!")
+Place.create(name: "Wingless Bar", address: "103 Yummy Street, Houston, TX 77054", description: "Wings are tasty and price is in the mid range!")
+Place.create(name: "Chocolate Everything", address: "107 Yummy Street, Houston, TX 77054", description: "The name speaks for itself! Chocolate covered everything and anything you can imagine!")
+Place.create(name: "SeaFood ToGo", address: "109 Yummy Street, Houston, TX 77054", description: "Great fast service! Seafood gets a fresh batch of lobsters everyday along with many more things!")
+Place.create(name: "World 360", address: "132 Yummy Street, Houston, TX 77054", description: "Food from around the world! Very pricy yet well worth it!")
+Place.create(name: "Sweet and Sour", address: "147 Yummy Street, Houston, TX 77054", description: "They have a menu for sweet food and sour food! You will be surprised on how many different items they have for you to try! Amazing customer service!")
+Place.create(name: "Waffles and Waffles", address: "209 Yummy Street, Houston, TX 77054", description: "24 hour 7 days a week every imaginable waffle served fresh and buffet on weekends! They also hold waffle competitions every twice a month!")
