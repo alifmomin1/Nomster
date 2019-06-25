ed8390107efdb8c9de59bacc1d66acd8e1dab948
Alif Momin <alifmomin1234567890@gmail.com>
2019-06-25 03:20:44 +0000

    Heroku Error Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
-    
+   config.action_mailer.default_url_options = { host: 'YOUR_WEBSITE_URL' } 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
